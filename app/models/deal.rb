e41007445ee50ba663c7507a594051f46f37b3d5
Mike Schwab <mike.schwab@gmail.com>
2018-08-10 17:54:03 -0400

    subcommissions calculated

--- app/models/deal.rb
+++ app/models/deal.rb
@@ -1,3 +1,60 @@
 class Deal < ApplicationRecord
   belongs_to :agent
+  has_many :participants
+  
+  def subcommissions
+    package = Hash.new 0
+    participants.leading.each do |participant|
+      package[participant.name] += lead_commission / participants.leading.count
+    end
+    
+    participants.interviewing.each do |participant|
+      package[participant.name] += interview_commission / participants.interviewing.count
+    end
+    
+    participants.showing.each do |participant|
+      package[participant.name] += show_commission / participants.showing.count
+    end
+    
+    participants.closing.each do |participant|
+      package[participant.name] += close_commission / participants.closing.count
+    end
+    package
+  end
+  
+  def distributable_commission
+    commission * 0.5
+  end
+  
+  def lead_commission
+    lead_rate * distributable_commission
+  end
+  
+  def interview_commission
+    interview_rate * distributable_commission
+  end
+  
+  def show_commission
+    show_rate * distributable_commission
+  end
+  
+  def close_commission
+    close_rate * distributable_commission
+  end
+  
+  def lead_rate
+    0.20
+  end
+  
+  def interview_rate
+    0.25
+  end
+  
+  def show_rate
+    0.30
+  end
+  
+  def close_rate
+    0.25
+  end
 end
--- app/models/participant.rb
+++ app/models/participant.rb
@@ -3,4 +3,13 @@ class Participant < ApplicationRecord
   belongs_to :assistant
   enum :role => [:lead, :interview, :show, :close]
   enum :status => [:preliminary, :active, :removed]
+  
+  scope :leading, lambda { where :role => :lead }
+  scope :interviewing, lambda { where :role => :interview }
+  scope :showing, lambda { where :role => :show }
+  scope :closing, lambda { where :role => close }
+  
+  def name
+    "#{role} by #{assistant.first_name}"
+  end
 end
