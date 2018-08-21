f780518030dd0572437806626790c7ddb1107536
Mike Schwab <mike.schwab@gmail.com>
2018-08-21 13:32:23 -0400

    staging env

--- config/environments/staging.rb
+++ config/environments/staging.rb
@@ -72,7 +72,7 @@ Rails.application.configure do
   config.action_mailer.smtp_settings = {
     address:              'smtp.gmail.com',
     port:                 587,
-    domain:               'citipadscommission.com',
+    domain:               'commissulator-staging.herokuapp.com',
     user_name:            Rails.application.credentials.sending_email[:username],
     password:             Rails.application.credentials.sending_email[:password],
     authentication:       'plain',
