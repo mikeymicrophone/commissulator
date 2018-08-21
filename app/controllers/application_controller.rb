d09662a6322289d5625d0291ca70e894a1df9bd9
Mike Schwab <mike.schwab@gmail.com>
2018-08-21 14:22:58 -0400

    collect agent name during registration

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,13 @@
 class ApplicationController < ActionController::Base
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  protected
+
+    def configure_permitted_parameters
+      devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :last_name, :phone_number])
+    end
+  
+    def after_sign_in_path_for resource
+      commissions_path
+    end
 end
