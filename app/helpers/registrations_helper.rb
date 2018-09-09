130a83cd18b3e4aaa29e0cbe2fcaf363c108fada
Mike Schwab <mike.schwab@gmail.com>
2018-09-08 22:24:06 -0400

    fabricate registration link not in production

--- app/helpers/registrations_helper.rb
+++ app/helpers/registrations_helper.rb
@@ -6,4 +6,8 @@ module RegistrationsHelper
   def size_options
     options_for_select Registration::APARTMENT_SIZES
   end
+  
+  def fabricate_registration_link
+    link_to 'Fabricate Registration', fabricate_registrations_path, :method => :post unless Rails.env.production?
+  end
 end
