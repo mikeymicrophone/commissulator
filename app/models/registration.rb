16a2629e3ea98aec8cd0329e148a2177952771b4
Mike Schwab <mike.schwab@gmail.com>
2018-09-16 15:29:43 -0400

    registered clients go to fub

--- app/models/registration.rb
+++ app/models/registration.rb
@@ -14,4 +14,26 @@ class Registration < ApplicationRecord
   
   PRICES = (2..30).to_a.map { |num| num * 500 }.append(1250, 1750, 2250).sort
   APARTMENT_SIZES = ['Alcove Studio', 'Junior-1', 'Studio', 'One Bedroom', 'Junior-4', 'Convertible-2', 'Two Bedroom', 'Convertible-3', 'Classic-6', 'Three Bedroom', 'Classic-7', 'Convertible-4', 'Four Bedroom', 'Five Bedroom+']
+  
+  def fub_people
+    people = []
+    clients.each do |client|
+      person = FubClient::Person.new :firstName => client.first_name, :lastName => client.last_name, :stage => 'Registered'
+      person.phones = client.phones.map { |phone| {:value => phone.number, :type => phone.variety} } if client.phones.present?
+      person.emails = client.emails.map { |email| {:value => email.address, :type => email.variety} } if client.emails.present?
+      people << person
+    end
+    people
+  end
+  
+  def follow_up!
+    fub_people.each do |person|
+      begin
+        FubClient::Person.collection_path '/v1/people?deduplicate=true'
+        person.save
+      rescue NoMethodError => error
+        Rails.logger.debug error.inspect
+      end
+    end
+  end
 end
