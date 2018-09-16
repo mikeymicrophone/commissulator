6db7ef8016cab9e63a8e66f90efd9fb0f1ee48ed
Mike Schwab <mike.schwab@gmail.com>
2018-09-16 14:19:46 -0400

    fub people are closed when synced after commission

--- app/models/commission.rb
+++ app/models/commission.rb
@@ -73,7 +73,7 @@ class Commission < ApplicationRecord
     tenants.each do |tenant|
       first_name = tenant.name.split.first
       last_name = tenant.name.split[1..-1].join ' '
-      person = FubClient::Person.new :firstName => first_name, :lastName => last_name, :emails => [{:value => tenant.email}], :phones => [{:value => tenant.phone}]
+      person = FubClient::Person.new :firstName => first_name, :lastName => last_name, :emails => [{:value => tenant.email}], :phones => [{:value => tenant.phone}], :stage => 'Closed'
       people << person
     end
     people
