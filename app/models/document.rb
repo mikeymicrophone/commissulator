470d4adf12751ca701894ea73663cd6821c81fbf
Mike Schwab <mike.schwab@gmail.com>
2018-09-19 14:02:25 -0400

    remove symbols from payment tally

--- app/models/document.rb
+++ app/models/document.rb
@@ -5,5 +5,15 @@ class Document < ApplicationRecord
   
   has_one_attached :capture
   
+  after_save :remove_symbols, :if => :is_payment?
+  
   scope :commission_payments, -> { where :role => ['Proof of Commission Payment', 'Owner Pay Invoice'] }
+  
+  def remove_symbols
+    update_attribute :name, name.gsub('$', '').gsub(',', '') if name =~ /[$,]/
+  end
+  
+  def is_payment?
+    self.class.where(:id => id).commission_payments.include? self
+  end
 end
