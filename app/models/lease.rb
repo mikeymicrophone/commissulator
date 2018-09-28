14095220c055a89fd2ed055561caae0dc20337fa
Mike Schwab <mike.schwab@gmail.com>
2018-09-28 17:58:32 -0400

    don't make empty leases

--- app/models/lease.rb
+++ app/models/lease.rb
@@ -4,8 +4,8 @@ class Lease < ApplicationRecord
   has_many :clients, :through => :tenants
   has_one :commission
   belongs_to :registration, :optional => true
-  # validates :street_number, :presence => true
-  # validates :street_name, :presence => true
+  validates :street_number, :presence => true
+  validates :street_name, :presence => true
   
   def name
     "#{address} with #{landlord&.name}"
