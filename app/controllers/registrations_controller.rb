5495803483d997222c8cc6d4eff79c3f7d8b7c98
Mike Schwab <mike.schwab@gmail.com>
2018-10-01 15:39:40 -0400

    client can submit signature without login

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -1,4 +1,5 @@
 class RegistrationsController < ApplicationController
+  skip_before_action :authenticate_avatar!, :only => :update
   before_action :set_registration, only: [:show, :display, :card, :edit, :update, :destroy]
 
   def index
