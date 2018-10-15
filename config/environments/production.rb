99348e43e2907fec391bdc28f6e26678b0de9a69
Mike Schwab <mike.schwab@gmail.com>
2018-10-15 14:10:52 -0400

    url host config

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -79,6 +79,7 @@ Rails.application.configure do
     enable_starttls_auto: true
   }
   config.action_mailer.default_url_options = { host: 'citipadscommission.com' }
+  Rails.application.routes.default_url_options = { :host => 'citipads.tech' }
 
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation cannot be found).
