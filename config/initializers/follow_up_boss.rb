0e6ac5e306ade4bbfe8881faee6b87c1741887c4
Mike Schwab <mike.schwab@gmail.com>
2018-10-19 16:21:02 -0400

    fub credential

--- config/initializers/follow_up_boss.rb
+++ config/initializers/follow_up_boss.rb
@@ -1,9 +1,4 @@
-if Rails.env.staging? || Rails.env.development?
-  FubClient::Client.instance.api_key = Rails.application.credentials.follow_up_boss[:staging_api_key]
-else
-  FubClient::Client.instance.api_key = Rails.application.credentials.follow_up_boss[:api_key]
-end
-
+FubClient::Client.instance.api_key = Rails.application.credentials.follow_up_boss[:api_key]
 
 module FubClient
   class Task < Resource
