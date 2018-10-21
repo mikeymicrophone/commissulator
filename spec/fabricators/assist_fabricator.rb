0b2f4468b8065b564f92b2d22bc316592097be30
Mike Schwab <mike.schwab@gmail.com>
2018-10-21 10:56:42 -0400

    fabricate flow uses existing agents

--- spec/fabricators/assist_fabricator.rb
+++ spec/fabricators/assist_fabricator.rb
@@ -1,6 +1,6 @@
 Fabricator :assist do
-  deal { find_or_fabricate :deal }
-  agent { find_or_fabricate :agent }
+  deal { Fabricate :deal }
+  agent
   role { Assist.roles.keys.sample }
   status :active
 end
