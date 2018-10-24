bbb7039e45713833e0ea35053bd830cc8ed78ff3
Cire DIA <ciremoussadia@gmail.com>
2018-10-24 21:06:59 +0000

    Only admin avatar can edit or delete an employer

--- app/helpers/employers_helper.rb
+++ app/helpers/employers_helper.rb
@@ -1,2 +1,13 @@
 module EmployersHelper
+    def edit_employer_link employer
+        if current_avatar.admin?
+            link_to fa_icon(:highlighter), edit_employer_path(employer), :title => 'Edit'
+        end
+    end
+
+    def delete_employer_link employer
+        if current_avatar.admin?
+            link_to fa_icon(:backspace), employer, method: :delete, data: { confirm: 'Are you sure?' }, :title => 'Delete'
+        end
+    end
 end
